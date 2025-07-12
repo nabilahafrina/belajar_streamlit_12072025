e074228d4060f47375f36247d779ecb30f69c233
nabilahafrina <nabilahafrina2@gmail.com>
2025-07-12 09:11:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 
 st.markdown("*Streamlit* is **really** ***cool***.")
 
-chart_data = id.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
