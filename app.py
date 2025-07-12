4e56186a61de105265db22e061734237c42f1d3c
nabilahafrina <nabilahafrina2@gmail.com>
2025-07-12 09:12:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
 
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
-
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-
-st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
-
-st.markdown("*Streamlit* is **really** ***cool***.")
-
-import streamlit as st
 import pandas as pd
 import numpy as np
 
